33fdb7d61ec2d70d2ba746c84d58a941f65bf67b
T-Welsh <85111929+T-Welsh@users.noreply.github.com>
2025-06-17 21:44:39 +0100

    Merge nvim dotfiles from work-dotfiles branch

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,15 +6,33 @@ vim.cmd("set shiftwidth=4")
 -- Set line numbers
 vim.opt.number = true -- Show absolute line number on current line
 vim.api.nvim_create_autocmd("InsertEnter", {
-	callback = function()
-		vim.opt.relativenumber = false -- Absolute numbers in insert mode
-	end,
+    callback = function()
+        vim.opt.relativenumber = false -- Absolute numbers in insert mode
+    end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-	callback = function()
-		vim.opt.relativenumber = true -- Relative numbers in normal mode
-	end,
+    callback = function()
+        vim.opt.relativenumber = true -- Relative numbers in normal mode
+    end,
 })
+
+-- Floating diagnostics on current line
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+
+-- Enable clipboard for mouse cut/paste.
+-- Enable clipboard support via win32yank
+vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+        ["+"] = "win32yank.exe -o --lf",
+        ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+}
 
 require("config.lazy")
