eecd5fdd87f5d91e142ed372b89957d1d5d2687f
T-Welsh <85111929+T-Welsh@users.noreply.github.com>
2025-06-17 22:08:20 +0100

    Fixed clipboard for Arch

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,15 +6,15 @@ vim.cmd("set shiftwidth=4")
 -- Set line numbers
 vim.opt.number = true -- Show absolute line number on current line
 vim.api.nvim_create_autocmd("InsertEnter", {
-    callback = function()
-        vim.opt.relativenumber = false -- Absolute numbers in insert mode
-    end,
+	callback = function()
+		vim.opt.relativenumber = false -- Absolute numbers in insert mode
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-    callback = function()
-        vim.opt.relativenumber = true -- Relative numbers in normal mode
-    end,
+	callback = function()
+		vim.opt.relativenumber = true -- Relative numbers in normal mode
+	end,
 })
 
 -- Floating diagnostics on current line
@@ -22,17 +22,5 @@ vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line Diagn
 
 -- Enable clipboard for mouse cut/paste.
 -- Enable clipboard support via win32yank
-vim.g.clipboard = {
-    name = "win32yank-wsl",
-    copy = {
-        ["+"] = "win32yank.exe -i --crlf",
-        ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-        ["+"] = "win32yank.exe -o --lf",
-        ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-}
 
 require("config.lazy")
